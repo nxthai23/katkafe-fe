2b139b558f1e320a811c05e126fdb850857d7e3e
Minhhoang <hoangminh881997@gmail.com>
2024-05-23 17:52:11 +0700

    feat: update active card assign

--- src/components/ui/StaffCardAssign.tsx
+++ src/components/ui/StaffCardAssign.tsx
@@ -21,14 +21,19 @@ const StaffCard = ({ cat, active, handleClick }: Props) => {
 
   return (
     <div
-      className={`rounded-xl border-solid border-[#4e4837] border-[3px] h-full w-full cursor-pointer relative ${
-        active ? "border-[#FC9B53]" : ""
-      }`}
+      className="rounded-xl border-solid border-[#4e4837] border-[3px] h-full w-full cursor-pointer relative"
       onClick={() => {
         handleClick?.(cat._id);
       }}
     >
       <div className="rounded-xl border-solid border-orange-20 border-[3px] h-full w-full">
+        {active && (
+          <div className="active-overlay relative">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <img src="/images/active.png" alt="" className="w-4 h-4" />
+            </div>
+          </div>
+        )}
         <div className="rounded-lg border-solid border-[#b2b19a] border h-full w-full flex flex-col justify-between relative">
           <div className="bg-[url('/images/background-cat.png')] bg-center bg-no-repeat bg-contain h-full flex items-end justify-center">
             <div className="flex justify-center relative">
